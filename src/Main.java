b6367c6d52dcb0fde5d0c743618e4396ed095f90
maxzdosreis <maxzdosreis@gmail.com>
2024-07-08 23:08:16 -0300

    Criação das Classes (Mentoria e Curso)

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+import dominio.Curso;
+import dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição Curso JavaScript");
+        curso2.setCargaHoraria(12);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição Mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
